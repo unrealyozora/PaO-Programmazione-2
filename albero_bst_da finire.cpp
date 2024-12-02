4cd1ed94dc5840e61f3d958fd08fb339a028d4d5
unrealyozora <tommaso.ceron@gmail.com>
2024-12-02 10:20:00 +0100

    albero bst rotto nel culo

--- albero_bst_da finire.cpp
+++ albero_bst_da finire.cpp
@@ -4,9 +4,15 @@ template <class T> class AlbBinRic;
 template <class T> class Nodo;
 
 template <class T>
+ostream& operator<< (ostream&, const AlbBinRic<T>&);
+
+template <class T>
+ostream& operator<< (ostream&, Nodo<T>*);
+
+template <class T>
 class Nodo{
     friend class AlbBinRic<T>;
-    friend ostream& operator<< (ostream&, Nodo<T>*);
+    friend ostream& operator<<  (ostream&, Nodo<T>*);
     private:
         T info;
         Nodo* sinistro;
@@ -16,7 +22,7 @@ class Nodo{
 };
 
 template <class T>
-ostream& operator<<( ostream& os, Nodo<T>* p){
+ostream& operator<< ( ostream& os, Nodo<T>* p){
     if (!p){
         os<< '@';
     }else{
@@ -27,6 +33,7 @@ ostream& operator<<( ostream& os, Nodo<T>* p){
 
 template <class T>
 class AlbBinRic{
+    friend ostream& operator<< (ostream&, const AlbBinRic<T>&);
     public:
         AlbBinRic(): radice(0) {};
         Nodo<T>* Find(T) const;
@@ -34,19 +41,19 @@ class AlbBinRic{
         Nodo <T>* Massimo () const;
         Nodo <T>* Succ (Nodo <T>*) const;
         Nodo <T>* Prec (Nodo <T>*) const;
-        void insert(T);
-        static T Valore(Nodo<T>* p){return p.info};
+        void Insert(T);
+        static T Valore(Nodo<T>* p){return p->info;}
     private:
         Nodo <T>* radice;
-        static Nodo<T>* FindRic (Nodo<T>*, T);
-        static Nodo<T>* MinimoRic(Nodo<T>*);
+        static Nodo<T>* FindRic (Nodo<T>*, T);   
+        static Nodo<T>* MinimoRic(Nodo<T>*);    
         static Nodo<T>* MassimoRic(Nodo<T>*);
         static void InsertRic (Nodo<T>*, T);
 };
 
 
 template <class T>
-Nodo<T>* AlbBinRic<T>::Find(T v) const{
+Nodo<T>* AlbBinRic<T>:: Find(T v) const {
     return FindRic(radice, v);
 }
 
@@ -97,6 +104,81 @@ Nodo<T>* AlbBinRic<T>::MassimoRic(Nodo<T>* x){
     }else{
         return MassimoRic(x->destro);
     }
+}
+
+template <class T>
+Nodo<T>* AlbBinRic<T>:: Succ (Nodo <T>* x) const{
+    if (!x){
+        return 0;
+    }
+    if (x->destro){
+        return MinimoRic(x->destro);
+    }
+    while(x->padre && x->padre->sinistro==x){
+        x=x->padre;
+    }
+    return x->padre;
+}
+
+template <class T>
+Nodo<T>* AlbBinRic<T>:: Prec (Nodo <T>* x) const{
+    if (!x){
+        return 0;
+    }
+    if (x->sinistro){
+        return MinimoRic(x->sinistro);
+    }
+    while(x->padre && x->padre->destro==x){
+        x=x->padre;
+    }
+    return x->padre;
+}
+
+template <class T>
+void AlbBinRic<T>:: Insert(T v){
+    if (!radice){
+        radice= new Nodo<T>(v);
+    }else{
+        InsertRic(radice, v);
+    }
+}
+
+template <class T>
+void AlbBinRic<T>:: InsertRic(Nodo<T>* x, T v){
+    if (v<x->info){
+        if (x->sinistro==0){
+            x->sinistro= new Nodo<T>(v,x);
+        }else{
+            InsertRic(x->sinistro, v);
+        }
+    }else{
+        if (x->destro==0){
+            x->destro= new Nodo<T>(v,x);
+        }else{
+            InsertRic(x->destro, v);
+        }
+    }
+}
+
+template <class T>
+ostream& operator<< (ostream& os, AlbBinRic<T>& a){
+    os<<a.radice<<endl;
+    return os;
+}
+
+
+int main(){
+    AlbBinRic<int> a;
+    a.Insert(3);
+    a.Insert(2);
+    a.Insert(3);
+    a.Insert(1);
+    a.Insert(6);
+    a.Insert(5);
+
+    cout<<"L'albero è:" << a << endl;
+
+    cout<< "Massimo: " << AlbBinRic<int>::Valore(a.Massimo()) << endl;
 }
 
 
